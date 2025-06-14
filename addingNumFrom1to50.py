0dbf0101b7785314a8a85497d3609c22a7462f20
sabyasachi125 <69254418+sabyasachi125@users.noreply.github.com>
2025-06-14 22:56:16 +0530

    Add files via upload

---
+++ addingNumFrom1to50.py
@@ -0,0 +1,6 @@
+x=range(1,51)
+sum=0
+for i in x:
+    sum=sum+i
+print(sum)
+    
---
+++ printEvenorOdd.py
@@ -0,0 +1,7 @@
+num=input("Enter a number :\n")
+num=int(num)
+
+if (num%2==0):
+    print("This is an even number:",num)
+else:
+    print("This is a odd number:",num)
